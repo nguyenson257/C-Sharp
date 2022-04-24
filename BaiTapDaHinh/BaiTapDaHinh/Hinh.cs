ca516782a6afde96a32d4068ad5469872d763841
NguyenHongSon <nguyenson25701@gmail.com>
2022-04-24 23:28:39 +0700

    first commit

---
+++ BaiTapDaHinh/BaiTapDaHinh/Hinh.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapDaHinh
+{
+    class Hinh
+    {
+        protected string Ten;
+        protected string ThuocTinh;
+    }
+
+    class TamGiac : Hinh
+    {
+        public TamGiac()
+        {
+            Ten = "Hinh Tam Giac";
+            ThuocTinh = "La1 da giac co 3 dinh khong thang hang va 3 canh noi 3 dinh do.";
+        }
+    }
+
+    class TuGiac : Hinh
+    {
+        public TuGiac()
+        {
+            Ten = "Hinh Tu Giac";
+            ThuocTinh = "La 1 da giac co 4 dinh va 4 canh.";
+        }
+    }
+    class BinhHanh : Hinh
+    {
+        public BinhHanh()
+        {
+            Ten = "Hinh Binh Hanh";
+            ThuocTinh = "La 1 tu giac duoc tao thanh khi 2 cap duong thang song song va cat nhau";
+        }
+    }
+    class ChuNhat : Hinh
+    {
+        public ChuNhat()
+        {
+            Ten = "Hinh Chu Nhat";
+            ThuocTinh = "La 1 tu giac loi co 4 goc vuong.";
+        }
+    }
+    class Vuong : Hinh
+    {
+        public Vuong()
+        {
+            Ten = "Hinh Vuong";
+            ThuocTinh = "La 1 tu giac co 4 canh bang nhau vaf 4 goc bang nhau.";
+        }
+    }
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chon hinh de xem thong tin:");
+            Console.WriteLine("1. Hinh tam giac");
+            Console.WriteLine("2. Hinh tu giac");
+            Console.WriteLine("3. Hinh binh hanh");
+            Console.WriteLine("4. Hinh chu nhat");
+            Console.WriteLine("5. Hinh vuong");
+
+        }
+    }
+}
---
+++ Baitap/BaiKiemTra1/BaiKiemTra1/BaiKiemTra1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiKiemTra1
+{
+    internal class BaiKiemTra1
+    {
+        public void Bai1()
+        {
+            Console.WriteLine("Bai 1:");
+            Console.WriteLine("Nhap vao 2 so: ");
+            Console.Write("n1 = ");
+            Double n1_46 = Double.Parse(Console.ReadLine()); 
+            Console.Write("n2 = ");
+            Double n2_46 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tong cua 2 so tren la: " + n1_46 + " + " + n2_46 + " = " + (n1_46 + n2_46));
+            Console.WriteLine("Hieu cua 2 so tren la: " + n1_46 + " - " + n2_46 + " = " + (n1_46 - n2_46));
+            Console.WriteLine("Tich cua 2 so tren la: " + n1_46 + " * " + n2_46 + " = " + (n1_46 * n2_46));
+            if(n2_46 == 0)
+                Console.WriteLine("Khong the chia voi so 0 ");
+            else
+                Console.WriteLine("Thuong cua 2 so tren la: " + n1_46 + " / " + n2_46 + " = " + (n1_46 / n2_46));
+            Console.ReadKey();
+        }
+
+        public void Bai2()
+        {
+            Console.WriteLine("Bai 2:");
+            Console.WriteLine("La co Viet Nam");
+            Console.WriteLine("========================================");
+            Console.WriteLine("=                                      =");
+            Console.WriteLine("=                                      =");
+            Console.WriteLine("=                  +                   =");
+            Console.WriteLine("=                 + +                  =");
+            Console.WriteLine("=          + + + + + + + + +           =");
+            Console.WriteLine("=            + + + + + + +             =");
+            Console.WriteLine("=              + + + + +               =");
+            Console.WriteLine("=             +  +   +  +              =");
+            Console.WriteLine("=            +           +             =");
+            Console.WriteLine("=                                      =");
+            Console.WriteLine("=                                      =");
+            Console.WriteLine("========================================");
+            Console.ReadKey();
+        }
+        public void Bai3()
+        {
+            Console.WriteLine("Bai 3:");
+            Console.Write("Nhap vao 1 so thap phan: ");
+            int n_46 = int.Parse(Console.ReadLine());
+            int i_46;
+            int[] nArray_46 = new int[10];
+            for (i_46 = 0; n_46 > 0; i_46++)
+            {
+                nArray_46[i_46] = n_46 % 2;
+                n_46 = n_46 / 2;
+            }
+            Console.Write("So thap phan sau khi chuyen doi: ");
+            for (i_46 = i_46 - 1; i_46 >= 0; i_46--)
+            {
+                Console.Write(nArray_46[i_46]);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        public void Bai4()
+        {
+            Console.WriteLine("Bai 4:");
+            Console.WriteLine("-----Thong tin sinh vien-----");
+            Console.WriteLine("Ho va ten: Nguyen Hong Son");
+            Console.WriteLine("Lop: 19T1");
+            Console.WriteLine("Ma sinh vien: 1911505310146");
+            Console.ReadKey();
+        }
+    }
+    
+    class program
+    {
+        static void Main(string[] args)
+        {
+            BaiKiemTra1 kt1 = new BaiKiemTra1();
+            kt1.Bai1();
+            kt1.Bai2();
+            kt1.Bai3();
+            kt1.Bai4();
+        }
+    }
+}
